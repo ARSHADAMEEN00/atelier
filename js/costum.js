3065a87b8bb24032beaa6be2518c8b0bb6ef458e
Arshad Koori <arshadk@localhost.localdomain>
2021-11-04 06:07:46 -0400

    first commit

---
+++ js/costum.js
@@ -0,0 +1,8 @@
+$('.slick-slide').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow:'<i class="fas fa-arrow-left"></i>',
+    nextArrow:'<i class="fas fa-arrow-left"></i>'
+  });
